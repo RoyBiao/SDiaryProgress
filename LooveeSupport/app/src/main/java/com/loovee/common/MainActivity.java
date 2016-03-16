2ab0ccc37ede2cb0389987b27c95ba1428cde2aa
ruibiao.li <273824978@qq.com>
2016-03-16 19:52:28 +0800

    调通LooveeSupport

--- LooveeSupport/app/src/main/java/com/loovee/common/MainActivity.java
+++ LooveeSupport/app/src/main/java/com/loovee/common/MainActivity.java
@@ -3,27 +3,23 @@ package com.loovee.common;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.loovee.common.app.LooveeApplication;
 import com.loovee.common.util.LogUtils;
-import com.loovee.common.xmpp.core.User;
 import com.loovee.common.xmpp.core.UserAuthentication;
 import com.loovee.common.xmpp.core.XMPPConnection;
 import com.loovee.common.xmpp.core.XMPPException;
-import com.loovee.common.xmpp.utils.XMPPUtils;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) this.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    }
 
+    public void mainClick(View view){
+        switch (view.getId()){
+            case R.id.login:
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
@@ -36,14 +32,17 @@ public class MainActivity extends AppCompatActivity {
                             if (connection.isConnected()) {
                                 LogUtils.jLog().i("1111111116");
                             }
-                            connection.login("13824125203", "123456", UserAuthentication.LoginType.tel);
+                            connection.login("8613824125203", "123456", UserAuthentication.LoginType.tel);
                         } catch (XMPPException e) {
                             e.printStackTrace();
                         } finally {
                         }
                     }
                 }).start();
-            }
-        });
+                break;
+
+            case R.id.register:
+                break;
+        }
     }
 }
